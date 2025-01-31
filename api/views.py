70fc9029500c1c1739afecf84d7c32d7e3091a3f
Shoyebo Moses <shoyebo98@gmail.com>
2025-01-31 23:43:23 +0100

    Initial Commit for HNG Stage 0 api

--- api/views.py
+++ api/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from datetime import datetime
-import pytz
+from datetime import datetime, timezone
+# import pytz
 
 
 # Create your views here.
 def stage0_view(request):
     response_data = {
         "email": "shoyebo98@gmail.com",
-        "current_datetime": datetime.now(pytz.UTC).isoformat(),
+        "current_datetime": datetime.now(timezone.utc).isoformat(),
+        # "current_datetime": datetime.now.isoformat(),
         "github_url": "https://github.com/ShoyeboMoses/HNG.git"
     }
     return JsonResponse(response_data)
